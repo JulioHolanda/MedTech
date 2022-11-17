4480f889be65dbd08d195195f2b3056a24bf1387
JulioHolanda <juliophc@msn.com>
2022-11-16 21:34:58 -0300

    Reajuste de Pastas e Bdd

--- lembretes_contagem.c
+++ lembretes_contagem.c
@@ -1,37 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <Windows.h>
 #include <unistd.h>
+#include <time.h>
 
-int main() {
+int main(void) {
+  
+  struct tm *data_hora_atual;  
+    
+  time_t segundos;
+   
+  time(&segundos);   
+  data_hora_atual = localtime(&segundos);
+
 
   int hor, min;
-  float intervalo, ms =1.0;
+  float intervalo;
+  int ms=1;
   int quant_remedio, dose_remedio;
   int pastTime=0, tomados=0, intervaloINT;
 
-  printf("\n\nVamos marcar o intervalo de tempo que será preciso no tratamento!");
-  printf("\n\nDigite o número de horas: ");
-  scanf("%d", &hor);
-  printf("\n\nDigite o número de minutos: ");
-  scanf("%d", &min);
-  printf("\n\nDe quantas em quantas horas precisará tomar o remédio (em horas): ");
+  printf("\n\nVamos marcar o intervalo de tempo que serÃ¡ preciso no tratamento!");
+  
+  printf("\n\nDe quantas em quantas horas precisarÃ¡ tomar o remÃ©dio? ");
+  printf("\nDigite aqui:");
   scanf("%f", &intervalo);
-  printf("\n\nDigite a quantidade (total) de remédios que terá de tomar:");
+  
+  printf("\n\nDigite a quantidade(total) de remÃ©dios(comprimidos, copinhos) que precisarÃ¡ tomar:");
   scanf("%d", &quant_remedio);
-  printf("\n\nDigite a dose de remédios que terá de tomar a cada chamada:");
+  printf("\n\nDigite a dose de remÃ©dios que terÃ¡ de tomar a cada chamada:");
   scanf("%d", &dose_remedio);
 
   intervalo = 60*intervalo;
 
   intervaloINT = (int)intervalo;
 
-printf(" %d", intervaloINT);
+  printf(" %d", intervaloINT);
 
-  //----------------------------Se o usuário digitar um valor acima de 60
-  // para
-  // os minutos, este será enviado para as horas transformando-a em valor
-  // equivalente-------------
+  // transformando hora e minuto em variÃ¡veis
+
+  hor=data_hora_atual->tm_hour;
+  min=data_hora_atual->tm_min;
+
+  system("clear");
+  printf("\nHora atual ........: %02d:%02d",hor, min);//hora e minuto atual
+
   if (min % 60 == 0) {
     hor = ((hor + (min / 60)));
     min = 0;
@@ -41,11 +53,7 @@ printf(" %d", intervaloINT);
       min = ((min % 60));
     }
   }
-
-   // Início do código do cronometro
-  printf("\n\n%02d:%02d", hor, min);
-  sleep(ms);
-
+  
   while(1){
 
     min+=10;
@@ -57,32 +65,24 @@ printf(" %d", intervaloINT);
             hor=00;
         }
     }
-    printf("\n\n%02d:%02d", hor, min);
+    system("clear");
+    printf("\n\nHora atual ........: %02d:%02d", hor, min);
+    sleep(ms);
 
     if (pastTime >= intervaloINT) {
-      //system("clear");
-      printf("\nHora de tomar o remédio!");
-      //system("clear");
+      system("clear");
+      printf("\n\nHora de tomar o remÃ©dio!");
       pastTime = 0;
       tomados += dose_remedio;
 
       if(tomados == quant_remedio){
-        printf("\n - Fim do alarme - \n");
+        printf("\n - Fim do tratamento - \n");
         exit(0);
       }
     }
-    //system("clear");
     sleep(ms);
 
     }
-    /* -------- Quando (minutos) chega a 60 incrementamos +1 para (horas) e
-     * zera a parte dos minutos ----------- */
-
-
-
-  /* ---------------------------------------------------------------------------
-   * Fim do código do cronometro
-   * ------------------------------------------------------------------ */
 
   return 0;
 }
